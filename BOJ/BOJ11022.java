8c4d0ef77804be399676a8c2a213bb4914f10787
Daewook Kwon <37796005+Daewooki@users.noreply.github.com>
2019-11-19 23:37:07 +0900

    BOJ #11022

---
+++ BOJ/BOJ11022.java
@@ -0,0 +1,15 @@
+import java.util.*;
+class BOJ11022{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println("Case #"+(i+1)+": "+a+" + " + b + " = " + (a+b));
+            //Case #1: 1 + 1 = 2
+        }
+        
+    }
+}
