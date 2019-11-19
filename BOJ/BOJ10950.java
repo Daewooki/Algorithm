df08cb9ab5ad8a0293887ab19045bb46c0e612ed
Daewook Kwon <37796005+Daewooki@users.noreply.github.com>
2019-11-19 23:32:46 +0900

    BOJ #10950

---
+++ BOJ/BOJ10950.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class BOJ10950{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i=0; i<n; i++){
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a+b);
+        }
+        
+    }
+}
