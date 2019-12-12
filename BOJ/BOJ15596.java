9246903e84e400539e3e65a0e8c2504c6015078d
Daewook Kwon <37796005+Daewooki@users.noreply.github.com>
2019-12-12 15:32:28 +0900

    BOJ #15596

---
+++ BOJ/BOJ15596.java
@@ -0,0 +1,10 @@
+// long sum(int[] a)  함수 구현 문제.
+public class BOJ15596.java {
+    long sum(int[] a) {
+        long ans = 0;
+        for(int z : a){
+            ans+=z;
+        }
+        return ans;
+    }
+}
